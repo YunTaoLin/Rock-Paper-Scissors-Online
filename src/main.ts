72ce0e4cb934d708702b29ba1d708be14a0801e9
YunTaoLin <404200173@gapp.fju.edu.tw>
2022-04-16 20:58:15 +0800

    新增PWA配置

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 
-let app = createApp(App)
+let app = createApp(App);
 
 if (import.meta.env.MODE == "local" || import.meta.env.MODE == "dev") {
-    app.config.performance  = true;
-    console.log("測試中");
-  } else {
-    app.config.performance = false;
-  }
+  app.config.performance = true;
+  console.log("測試中");
+} else {
+  app.config.performance = false;
+}
 
-app.use(router).use(store).mount('#app')
+app.use(router).use(store).mount("#app");
+serviceWorkerRegistration.register();
